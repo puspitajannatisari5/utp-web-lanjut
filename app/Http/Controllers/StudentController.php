99b1b620d7d3bc792c3feab77ea9534af66d861e
puspitajannatisari5 <puspitasari7980@gmail.com>
2025-05-16 15:55:05 +0700

    Part 4

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
 use App\Models\Student;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class StudentController extends Controller
@@ -15,24 +16,22 @@ class StudentController extends Controller
      */
     public function index(): View
     {
-        $students = Student::all();
+        $students = Student::all(); 
         return view('students.index')->with('students', $students);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-
         return view('students.create');
-
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $input = $request->all();
         Student::create($input);
@@ -42,32 +41,38 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): View
     {
-        //
+        $students = Student::find($id);
+        return view('students.show')->with('students', $students);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        $student = Student::find($id);
+        return view('students.edit')->with('students', $student);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): RedirectResponse
     {
-        //
+        $student = Student::find($id);
+        $input = $request->all();
+        $student->update($input);
+        return redirect('students')->with('flash_message', 'student Updated!');  
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): RedirectResponse
     {
-        //
+        Student::destroy($id);
+        return redirect('students')->with('flash_message', 'Student deleted!'); 
     }
 }
