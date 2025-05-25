57cd3ab3b6dc26afd4d9d2300dabc4f58851125a
puspitajannatisari5 <puspitasari7980@gmail.com>
2025-05-25 14:41:51 +0700

    Part 10

--- resources/views/Courses/show.blade.php
+++ resources/views/Courses/show.blade.php
@@ -3,17 +3,6 @@
  
  
 <div class="card">
-  <div class="card-header">Students Page</div>
-  <div class="card-body">
-   
- 
-        <div class="card-body">
-        <h5 class="card-title">Name : {{ $students->name }}</h5>
-    @extends('layout')
-@section('content')
- 
- 
-<div class="card">
   <div class="card-header">Course Page</div>
   <div class="card-body">
    
@@ -21,15 +10,7 @@
         <div class="card-body">
         <h5 class="card-title">Name : {{ $courses->name }}</h5>
         <p class="card-text">Syllabus : {{ $courses->syllabus }}</p>
-        <p class="card-text">Duration : {{ $courses->duration }}</p>
-  </div>
-       
-    </hr>
-  
-  </div>
-</div>
-@endsection    <p class="card-text">Address : {{ $students->address }}</p>
-        <p class="card-text">Mobile : {{ $students->mobile }}</p>
+        <p class="card-text">Duration : {{ $courses->duration() }}</p>
   </div>
        
     </hr>
